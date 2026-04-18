2d19a0e337d8188755299bb6865d7d3bcab518df
ADAM USMAN <95586155+kobicdroid@users.noreply.github.com>
2026-04-18 15:54:00 +0100

    Update greeting from 'Hello' to 'Goodbye'

--- portal.py
+++ portal.py
@@ -38,7 +38,7 @@ except:
 
 MAINTENANCE_MODE = True  
 ADMIN_SECRET_KEY = "SUMI" 
-TARGET_DATE = datetime(2026, 4, 18, 15, 10) 
+TARGET_DATE = datetime(2026, 4, 18, 16, 10) 
 
 # --- SESSION STATE ---
 if 'maintenance_bypass' not in st.session_state:
